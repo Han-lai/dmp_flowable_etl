26b4d47f74d493b90dadee94bbf515c4032050dd
Han-lai <sh41bee@gmail.com>
2026-03-02 13:38:50 +0800

    chore: stop tracking validation scripts and add to .gitignore

--- scripts/validation/gold_layer/check_st02_final.py
+++
@@ -1,6 +0,0 @@
-import clickhouse_connect
-
-client = clickhouse_connect.get_client(host='10.136.218.207', port=8121, username='default', password='default')
-r = client.query("SELECT region, plant, factory, line, count() FROM silver.mv_fact_task_vx WHERE line='ST02' GROUP BY region, plant, factory, line")
-for row in r.result_rows:
-    print(row)
--- scripts/validation/gold_layer/check_st02_view.py
+++
@@ -1,6 +0,0 @@
-import clickhouse_connect
-
-client = clickhouse_connect.get_client(host='10.136.218.207', port=8121, username='default', password='default')
-r = client.query("SELECT * FROM silver.mv_dim_mfg_five_level WHERE line_name='ST02'")
-for row in r.result_rows:
-    print(row)
